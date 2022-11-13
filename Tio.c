5f1e07622a04f6a93338c4e9f40343d9803b2872
ElJuanes09 <juanex210404@gmail.com>
2022-11-13 13:27:33 -0500

    DiosNoEsta

--- Tio.c
+++ Tio.c
@@ -13,3 +13,22 @@ Tio* newTio()
 
 
 
+int Ingresar_Dinero(int *cantidad)
+{
+    //disponible _ restatar
+    /*int i, n;
+    time_t t; 
+
+    n = 1;
+
+    srand((unsigned) time(&t));
+
+    for(i = 1; i<n; i++){
+      
+    }
+      */int valor = rand()%101;
+
+      cantidad = cantidad + valor;
+      
+    return cantidad;
+}
