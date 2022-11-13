f82ab4c34c346190e1bb281c4dc0095a9c11268b
Sebastian <juanseco012@gmail.com>
2022-11-13 13:25:44 -0500

    Yo solo se, que profe paseme el curso porfa

--- Primo.c
+++ Primo.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include "Primo.h"
-
+#include <stdlib.h>
 
 Primo* newPrimo()
 {
     return(Primo*)malloc(sizeof(Primo));
 }
 
+int Retirar_Dinero(int *cantidad)
+{
+    //disponible _ restatar
+    /*int i, n;
+    time_t t; 
 
+    n = 1;
+
+    srand((unsigned) time(&t));
+
+    for(i = 1; i<n; i++){
+      
+    }
+      */int valor = rand()%101;
+
+      cantidad = cantidad - valor;
+      
+    return cantidad;
+}
